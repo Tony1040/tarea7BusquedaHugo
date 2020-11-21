28f2891abd03c4a93b08731789da8dd983ae6b20
Anthony Ramírez López <arleyy1004@hotmail.com>
2020-11-20 21:00:33 -0600

    .

--- js/search.js
+++ js/search.js
@@ -21,18 +21,6 @@ var fuseOptions = {
   keys: [{
       name: "title",
       weight: 0.8
-    },
-    {
-      name: "content",
-      weight: 0.5
-    },
-    {
-      name: "tags",
-      weight: 0.3
-    },
-    {
-      name: "categories",
-      weight: 0.3
     }
   ]
 };
